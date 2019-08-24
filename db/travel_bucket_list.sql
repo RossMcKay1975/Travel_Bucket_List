5e7e96700a30d57d841cf8e4152140d2626388d4
Ross Mckay <ross.mckay1975@gmail.com>
2019-08-24 14:00:00 +0100

    db complete

--- db/travel_bucket_list.sql
+++ db/travel_bucket_list.sql
@@ -1,23 +1,22 @@
-DROP TABLE adventures;
+DROP TABLE adventure;
 DROP TABLE cities;
 DROP TABLE countries;
 
 
-CREATE TABLE cities (
+CREATE TABLE country (
   id SERIAL4 PRIMARY KEY,
-  city VARCHAR(255),
+  country_name VARCHAR(255)
+);
+
+CREATE TABLE city (
+  id SERIAL4 PRIMARY KEY,
+  city_name VARCHAR(255),
   country_id INT4 REFERENCES country(id)
 );
 
-CREATE TABLE countries (
-  id SERIAL4 PRIMARY KEY,
-  country VARCHAR(255)
-);
-
-CREATE TABLE adventures (
+CREATE TABLE adventure (
   id SERIAL4 PRIMARY KEY,
   city_id INT4 REFERENCES city(id),
-  country_id INT4 REFERENCES country(id),
   visited BOOLEAN,
-  rating INT4
+  RATING INT4
 );
